902785ca0932b28e63b687094db4a6153da7fc41
sjpbailey <sjpbailey1961@gmail.com>
2021-03-21 00:25:34 -0700

    Update tuya-switch-controller.py

--- tuya-switch-controller.py
+++ tuya-switch-controller.py
@@ -235,12 +235,13 @@ class Controller(polyinterface.Controller):
         #LOGGER.info(SWITCHKEY)
         LOGGER.info("TEST1 ID" + 'item'["id"]) # Device ID
         LOGGER.info("TEST1 KEY" + 'item'["key"]) # Device Key
-               
+        if 'id' is not None:
+            self.addNode(SwitchNodes1(self, self.address, 'tuyaswitch1', 'TreatLife-1', SWITCHID))       
 
 
     def discover(self,*args, **kwargs):    
-        if "id" is not None:
-            self.addNode(SwitchNodes1(self, self.address, 'tuyaswitch1', 'TreatLife-1', 'SWITCHID'))
+        #if "id" is not None:
+        #    self.addNode(SwitchNodes1(self, self.address, 'tuyaswitch1', 'TreatLife-1', 'SWITCHID'))
         if "id" is not None: 
             self.addNode(SwitchNodes2(self, self.address, 'tuyaswitch2', 'TreatLife-2'))
         
